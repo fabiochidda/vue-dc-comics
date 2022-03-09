1ff0c0b8b73b0e6172bf62f39391053a405e8102
fabiochidda <fabio.chidda99@gmail.com>
2022-03-09 02:25:02 +0100

    Aggiunti i componenti per Header, main e footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer>
+
+
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'MsFooter'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'MsHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
