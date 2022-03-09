34601b29d91a2a1a1db3c96fbaea634e8f3d84d2
fabiochidda <fabio.chidda99@gmail.com>
2022-03-10 00:31:28 +0100

    Aggiunto l'header

--- src/App.vue
+++ src/App.vue
@@ -28,8 +28,34 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  width: 100%;
+}
+
+.container-lg {
+  width: 1320px;
+  margin: 0 auto;
+}
+
+.row {
+  display: flex;
+}
+
+ul li {
+  list-style: none;
+
+  a {
+    text-decoration: none;
+  }
+  
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,122 @@
 <template>
     <header>
-        
+        <div class="container">
+            <div class="container-lg">
+                <figure class="header-logo">
+                    <img src="../assets/img/dc-logo.png" alt="">
+                </figure>
+                <nav class="nav-container">
+                    <ul class="nav">
+                        <li v-for="(el, i) in navLinks" :key="i" class="nav-links" :class=" navLinks[i].isActive ? 'active' : '' ">
+                            <a :href="el.href">{{el.text}}</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
     </header>
 </template>
 
 <script>
 export default {
-    name: 'MsHeader'
+    name: 'MsHeader',
+    data() {
+        return {
+            navLinks: [
+                {
+                    text: 'CHARACTERS',
+                    href: '#',
+                    isActive: true,
+                },
+                {
+                    text: 'COMICS',
+                    href: '#',
+                },
+                {
+                    text: 'MOVIES',
+                    href: '#'
+                },
+                {
+                    text: 'TV',
+                    href: '#'
+                },
+                {
+                    text: 'GAMES',
+                    href: '#'
+                },
+                {
+                    text: 'COLLECTIBLES',
+                    href: '#'
+                },
+                {
+                    text: 'VIDEOS',
+                    href: '#'
+                },
+                {
+                    text: 'FANS',
+                    href: '#'
+                },
+                {
+                    text: 'NEWS',
+                    href: '#'
+                },
+                {
+                    text: 'SHOP',
+                    href: '#'
+                }, 
+            ]
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+
+.container-lg{
+    display: flex;
+    min-height: 130px;
+    justify-content: space-between;
+}
+
+.header-logo {
+    align-self: center;
+}
+
+.nav-container {
+    display: flex;
+    gap: 25px;
+}
+
+.nav {
+    display: contents;
+}
+
+.nav-links {
+
+    a {
+        display: flex;
+        align-items: center;
+        height: 100%;
+        font-size: 15px;
+        font-weight: bold;
+        position: relative;
+    }
+
+    &.active {
+        a::after {
+            content: '';
+            display: block;
+            height: 5px;
+            position: absolute;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: #0282F9;
+        }
+    }
+
+}
+
+
 
 </style>
