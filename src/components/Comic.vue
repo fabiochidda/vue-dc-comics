c01c52d9713580f7f8a2ca63d765856271aad537
fabiochidda <fabio.chidda99@gmail.com>
2022-03-11 03:15:20 +0100

    Aggiunto componente comic ed importato nel main (non perfetto, ma funziona... circa)

---
+++ src/components/Comic.vue
@@ -0,0 +1,57 @@
+<template>
+
+    <div class="comic-card">
+        <figure class="comic-image">
+            <img :src="imgSrc" alt="">
+        </figure>
+
+        <div class="comic-title">
+            <h3>{{comicTitle}}</h3>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'MsComic',
+    props: {
+        imgSrc: {
+            type: String,
+            required: true
+        },
+        comicTitle: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.comic-card {
+    width: calc(100% / 6);
+    height: 150px;
+
+    .comic-image {
+        aspect-ratio: 1/1;
+        overflow: hidden;
+        
+        img {
+            display: block;
+            object-fit: cover;
+            object-position: top;
+        }
+    }
+}
+
+.comic-title {
+    h3 {
+        text-transform: uppercase;
+        color: white;
+    }
+}
+
+</style>
